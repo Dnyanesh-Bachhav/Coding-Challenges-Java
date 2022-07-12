ec8c95c0a56a2437f8f8555505561ca2212fcd27
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-07-12 12:04:28 +0530

    Some coding challenges...

---
+++ End_Of_File.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class End_Of_File{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
